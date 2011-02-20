1beead39585daf4c1e94e31901c558d0d69c25c8
unknown <BRUNO@.(none)>
2011-02-20 17:36:37 -0200

    weby with 3 links

--- weby.hs
+++ weby.hs
@@ -1,40 +1,31 @@
-import Prelude hiding (drop, head)
+import Prelude hiding (drop, head, length)
 
 data List a
     = Nil
-    | Cons a (Tree a) (List a) (List a)
-data Tree a
-    = Leaf
-    | Node (List a) (List a)
+    | Cons a (List a) (List a) (List a)
 
-height Leaf = 0
-height (Node (Cons _ ta _ _) (Cons _ tb _ _))
-    | ha == hb =  1 + ha
-    | otherwise = error "unbalanced tree"
-    where
-      ha = height ta
-      hb = height tb
+length Nil = 0
+length (Cons _ xs _ _) = 1 + length xs
 
-size Leaf = 1
-size (Node (Cons _ ta _ _) (Cons _ tb _ _))
-    | sa == sb =  1 + sa + sb
-    | otherwise = error "unbalanced tree"
-    where
-      sa = size ta
-      sb = size tb
-
-cons a Nil = Cons a Leaf Nil Nil
-cons a bs@(Cons b _ Nil _) = Cons a Leaf bs bs
-cons a bs@(Cons b tb cs@(Cons c tc ds _) _)
-    | height tb == height tc = Cons a (Node bs cs) ds bs
-    | otherwise = Cons a Leaf bs bs
+cons a Nil = Cons a Nil Nil Nil
+cons a bs@(Cons b _ _ Nil) = Cons a bs bs bs
+cons a bs@(Cons b _ _ es@(Cons e _ _ hs))
+    | length bs - length es == length es - length hs = Cons a bs es hs
+    | otherwise = Cons a bs bs bs
 
 head (Cons a _ _ _) = a
 
+drop :: Int -> List a -> List a
 drop 0 as = as
-drop n (Cons a t cs bs)
-    | size t  <= n = drop (n-size t) cs
-    | otherwise = drop (n-1) bs
+drop n (Cons a bs cs ds)
+    | n < las-lcs = drop (n-(las-lbs)) bs
+    | n < las-lds = drop (n-(las-lcs)) cs
+    | otherwise = drop (n-(las-lds)) ds
+      where
+        las = lbs + 1
+        lbs = length bs
+        lcs = length cs
+        lds = length ds
 
 pp n = map (\i -> head . drop i $ list) [0..n]
     where
